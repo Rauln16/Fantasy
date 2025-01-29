69b104829fc79a27a716f9dfd939ed82973d3add
Rauln16 <raulnietogarcia16@gmail.com>
2025-01-29 10:25:53 +0100

    Agregados componentes Dialog y Filter

---
+++ Fantasy/Fantasy.Frontend/Shared/ConfirmDialog.razor.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using MudBlazor;
+
+namespace Fantasy.Frontend.Shared;
+
+public partial class ConfirmDialog
+{
+    [CascadingParameter] private IMudDialogInstance MudDialog { get; set; } = null!;
+    [Parameter] public string Message { get; set; } = null!;
+
+    private void Accept()
+    {
+        MudDialog.Close(DialogResult.Ok(true));
+    }
+
+    private void Cancel()
+    {
+        MudDialog.Close(DialogResult.Cancel());
+    }
+}
